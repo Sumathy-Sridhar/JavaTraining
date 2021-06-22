11895645de887652ea71fcaf0d42c12a72aebcae
Sumathy-Sridhar <79184088+Sumathy-Sridhar@users.noreply.github.com>
2021-06-22 16:10:58 +0530

    Update RowColSum.java

--- RowColSum.java
+++ RowColSum.java
@@ -3,25 +3,21 @@ public class RowColSum
     public static void main(String[] args) {    
         int rows, cols, sumRow, sumCol;    
         int a[][] = {       
-                        {1, 2, 3},    
-                        {4, 5, 6},    
-                        {7, 8, 9}    
+                        {10,20,30},    
+                        {10,10,10},    
+                        {30,20,10}    
                     };    
           rows = a.length;    
         cols = a[0].length;    
         for(int i = 0; i < rows; i++){    
-            sumRow = 0;    
+            sumRow = 0;  
+              sumCol = 0;      
             for(int j = 0; j < cols; j++){    
-              sumRow = sumRow + a[i][j];    
+              sumRow = sumRow + a[i][j];
+	 sumCol = sumCol + a[j][i];        
             }    
             System.out.println("Sum of " + (i+1) +" row: " + sumRow);    
-        }    
-        for(int i = 0; i < cols; i++){    
-            sumCol = 0;    
-            for(int j = 0; j < rows; j++){    
-              sumCol = sumCol + a[j][i];    
-            }    
-            System.out.println("Sum of " + (i+1) +" column: " + sumCol);    
+           System.out.println("Sum of " + (i+1) +" column: " + sumCol);    
         }    
     }    
 }    
