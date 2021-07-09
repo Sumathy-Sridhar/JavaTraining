7dc1ab5cbb1823e9de74814258172dc3fe671f0c
Sumathy-Sridhar <79184088+Sumathy-Sridhar@users.noreply.github.com>
2021-07-09 10:46:00 +0530

    Add files via upload

---
+++ Day16/SwingCalculator.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+
+class SwingCalculator {
+ public static void main(String[] args) {
+  JFrame frame = new Calculator();
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  frame.setVisible(true);
+  }
+}
